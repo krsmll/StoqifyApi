0f8754f5073040fb552c466ef0d912d6926be203
Soumen Banerjee <soumen.banerjee@hansab.ee>
2021-12-10 13:06:20 +0200

    WMS-43 fixed test issue

--- src/test/java/com/knits/product/controller/GroupControllerTest.java
+++ src/test/java/com/knits/product/controller/GroupControllerTest.java
@@ -1,79 +1,70 @@
 package com.knits.product.controller;
 
-
-import com.knits.product.dto.GroupDto;
-import com.knits.product.dto.UserDto;
-import com.knits.product.entity.Group;
-import com.knits.product.mapper.GroupMapper;
-import com.knits.product.repository.GroupRepository;
-import com.knits.product.service.GroupService;
-import org.junit.Before;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.knits.product.dto.GroupDto;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = GroupController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class GroupControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-
-    @MockBean
-    private GroupController groupController;
-
-
-    @MockBean
-    private GroupService groupService;
-
-    @MockBean
-    private GroupRepository groupRepository;
-
-    @MockBean
-    private GroupMapper groupMapper;
-
-
-    //Testing Get all groups URL
-    @Test
-    void testShouldGetAllGroup() throws Exception {
-
-        GroupDto group = new GroupDto();
-        group.setId(1);
-        group.setName("Admin");
-
-        when(groupService.getGroupById(1)).thenReturn(group);
-        System.out.println(group);
-
-        String URL = "/api/groups/1";
-        mockMvc.perform(MockMvcRequestBuilders.get(URL))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.jsonPath("@.name").exists())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists())
-                .andExpect(status().isOk());
-
-    }
-
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
-    void testShouldReturnOkNoContent() throws Exception {
+    void test_to_get_all_groups() throws Exception {
 
+        String getGroupControllerResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/api/groups/all"))
+                .andReturn().getResponse().getContentAsString();
+
+        GroupDto[] getGroups = objectMapper.readValue(getGroupControllerResult, GroupDto[].class);
+
+        assertTrue(getGroups.length > 1);
     }
 
+    @Test
+    void test_to_get_single_group_data() throws Exception {
+        String getGroupControllerResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/api/groups/1"))
+                .andReturn().getResponse().getContentAsString();
 
+        GroupDto getGroupName = objectMapper.readValue(getGroupControllerResult, GroupDto.class);
+
+        assertEquals("Admin", getGroupName.getName());
+    }
+
+    @Test
+    void test_active_groups() throws Exception {
+        String getGroupControllerResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/api/groups/active"))
+                .andReturn().getResponse().getContentAsString();
+
+        GroupDto[] getActiveGroups = objectMapper.readValue(getGroupControllerResult, GroupDto[].class);
+
+        assertEquals(true, Arrays.stream(getActiveGroups).findFirst().get().getIsActive());
+        assertEquals("Admin", Arrays.stream(getActiveGroups).findFirst().get().getName());
+    }
+
+    @Test
+    void test_to_get_group_by_id() throws Exception {
+        String getGroupControllerResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/api/groups/1"))
+                .andReturn().getResponse().getContentAsString();
+
+        GroupDto getGroupData = objectMapper.readValue(getGroupControllerResult, GroupDto.class);
+
+        assertEquals(true, getGroupData.getIsActive());
+        assertEquals("Admin",getGroupData.getName());
+    }
 }
