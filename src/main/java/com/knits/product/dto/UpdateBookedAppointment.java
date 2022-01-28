9ddf83e4759361aa6bd31fd4891dd4112404c989
Soumen Banerjee <soumen.banerjee@hansab.ee>
2022-01-28 15:57:26 +0200

    added validation for dtos, modified exception controller

--- src/main/java/com/knits/product/dto/UpdateBookedAppointment.java
+++ src/main/java/com/knits/product/dto/UpdateBookedAppointment.java
@@ -3,6 +3,7 @@ package com.knits.product.dto;
 import lombok.Data;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class UpdateBookedAppointment {
@@ -13,6 +14,7 @@ public class UpdateBookedAppointment {
 
     @NotNull(message = "Status can not be null")
     @NotBlank(message = "Status can not be blank")
+    @Pattern(regexp = "^[A-Z]*$", message = "Appointment status should contain only character")
     private String status;
     private String comment;
 }
